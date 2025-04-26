3e121a2ac43020bf443129447be20cf065083478
andreyferraz <andreyferraz@unimedceara.com.br>
2025-04-26 05:53:51 -0300

    Add necessary imports and configure Produto model and AppServiceProvider

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto; // Importa o modelo Produto
+use Illuminate\Support\Facades\Redirect;
 
 class ProdutoController extends Controller
 {
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = ['nome', 'descricao']; // Campos que podem ser preenchidos em massa
+    protected $table = 'produtos'; // Nome da tabela no banco de dados
+    public $timestamps = true; // Habilita os timestamps (created_at e updated_at)
 }
